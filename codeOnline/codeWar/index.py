908a5309e0d8f03535e2f6036e399788a6988b24
hyquaq <hyquaq@gmail.com>
2021-01-29 18:10:39 +0700

    update: .

--- codeOnline/codeWar/index.py
+++ codeOnline/codeWar/index.py
@@ -1,25 +1,20 @@
-n = int(input('enter: '))
-
-i = 1
-ans = 0
-count = 0
-while True:
-    dk = True
-    for i in range(9,1,-1):
-        if not n % i:
+def digitsProduct(n):
+    ans = 0
+    for i in range(9, 1, -1):
+        while not n % i:
             ans = ans * 10 + i
             n //= i
-            dk = False
-            count += 1
-            break
     
-    if dk:
-        break
+    fake = ans
+    ans = 0
+    while fake > 0:
+        ans = ans * 10 + fake % 10
+        fake //= 10
 
-fake = ans
-ans = 0
-for i in range(count):
-    ans = ans * 10 + fake % 10
-    fake //= 10
+    if ans:
+        return ans 
+    return -1
 
-print(ans, count)
+n = int(input('enter: '))
+
+print(digitsProduct(n))
