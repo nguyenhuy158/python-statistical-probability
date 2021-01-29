355f699f4731da55e5c3466fca06a1f4d58862f9
hyquaq <hyquaq@gmail.com>
2021-01-29 17:36:13 +0700

    update: algrithm n!

---
+++ large n!/numberZeroOfn!.py
@@ -0,0 +1,26 @@
+n = int(input('enter n!: '))
+
+count = 0 # count zero
+produc = 1 # produc = n!
+
+for i in range(1, n+1):
+	produc *= i # n! = 1.2.3..
+
+	while not produc % 10: # n! = 120 -> 12	
+		count += 1
+		produc //= 10
+
+	produc %= 100 # n! = 3628800 -> 88
+
+print(f"number zero of {n}! is {count}")
+# test
+countT = 0
+produc = 1
+for i in range(1, n+1):
+	produc *= i
+
+while not produc % 10:
+	countT += 1
+	produc //= 10
+	
+print(f"number zero of {n}! is {countT}")
