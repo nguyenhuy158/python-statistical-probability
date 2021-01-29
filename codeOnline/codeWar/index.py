1db44a2156768ddf037a15f318a1084657d79d09
hyquaq <hyquaq@gmail.com>
2021-01-29 18:06:11 +0700

    update: tim so nho nhat co tich cac chu so = n cho trc

--- codeOnline/codeWar/index.py
+++ codeOnline/codeWar/index.py
@@ -1,3 +1,25 @@
-ch = 'a'
+n = int(input('enter: '))
 
-print(f"{ch}\t{ch}")
+i = 1
+ans = 0
+count = 0
+while True:
+    dk = True
+    for i in range(9,1,-1):
+        if not n % i:
+            ans = ans * 10 + i
+            n //= i
+            dk = False
+            count += 1
+            break
+    
+    if dk:
+        break
+
+fake = ans
+ans = 0
+for i in range(count):
+    ans = ans * 10 + fake % 10
+    fake //= 10
+
+print(ans, count)
