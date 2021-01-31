480224516620f9e86236b53897b509756dcba9f9
hyquaq <hyquaq@gmail.com>
2021-01-31 10:15:47 +0700

    update: code

--- large n!/numberZeroOfn!.py
+++ large n!/numberZeroOfn!.py
@@ -1,26 +1,30 @@
-n = int(input('enter n!: '))
+def numberZeroDigits(n):		
+	count = 0 # count zero
+	produc = 1 # produc = n!
 
-count = 0 # count zero
-produc = 1 # produc = n!
+	for i in range(1, n+1):
+		produc *= i # n! = 1.2.3..
 
-for i in range(1, n+1):
-	produc *= i # n! = 1.2.3..
+		while not produc % 10: # n! = 120 -> 12	
+			count += 1
+			produc //= 10
 
-	while not produc % 10: # n! = 120 -> 12	
-		count += 1
+		produc %= 100 # n! = 3628800 -> 88
+
+	print(f"number zero of {n}! is {count}")
+	# test
+	countT = 0
+	produc = 1
+	for i in range(1, n+1):
+		produc *= i
+
+	while not produc % 10:
+		countT += 1
 		produc //= 10
+		
+	return countT
 
-	produc %= 100 # n! = 3628800 -> 88
 
-print(f"number zero of {n}! is {count}")
-# test
-countT = 0
-produc = 1
-for i in range(1, n+1):
-	produc *= i
+n = int(input('enter n!: '))
+print(numberZeroDigits(n))
 
-while not produc % 10:
-	countT += 1
-	produc //= 10
-	
-print(f"number zero of {n}! is {countT}")
