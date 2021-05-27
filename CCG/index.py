f8cf6ce7a32de0dbc0d1b8ee86b487588fdea820
Nguyen Huy <hyquaq@gmail.com>
2021-05-27 19:39:55 +0700

    update 1.1

--- CCG/index.py
+++ CCG/index.py
@@ -1,10 +1,12 @@
 import random
 
+
 def saveScore(a, b):
-	fi = open('score.txt','w')
-	result = f'scorePlayer{a}\nscoreComputer{b}'
-	fi.write(result)
-	fi.close()
+    fi = open('score.txt', 'w')
+    result = f'scorePlayer{a}\nscoreComputer{b}'
+    fi.write(result)
+    fi.close()
+
 
 # set variable default
 scorePlayer = 0
@@ -17,25 +19,26 @@ print('\trule of game:')
 print('\t1: red\n\t2: green\n\t3: blue')
 
 while True:
-	#
-	choosePlayer = int(input('Enter you choose: '))
-	while choosePlayer not in [1, 2, 3]:
-		choosePlayer = int(input("Error I don't understand, again: "))
+    #
+    choosePlayer = input('Enter you choose: ')
+    while choosePlayer == '' or int(choosePlayer) not in [1, 2, 3]:
+        choosePlayer = input("Error I don't understand, again: ")
 
-	chooseComputer = random.randint(1,3)
-	print(f'\nyou choose {color.get(choosePlayer)}')
-	print(f'computer choose {color.get(chooseComputer)}')
-	if choosePlayer == chooseComputer:
-		scorePlayer += 1
-		print('You win')
-	else:
-		scoreComputer += 1
-		print('You miss')
-	print('score you: {}\nscore computer: {}'.format(scorePlayer, scoreComputer))
+    chooseComputer = random.randint(1, 3)
+    choosePlayer = int(choosePlayer)
+    print(f'\nyou choose {color.get(choosePlayer)}')
+    print(f'computer choose {color.get(chooseComputer)}')
+    if choosePlayer == chooseComputer:
+        scorePlayer += 1
+        print('You win')
+    else:
+        scoreComputer += 1
+        print('You miss')
+    print('score you: {}\nscore computer: {}'.format(scorePlayer, scoreComputer))
 
-	again = input('\n\nDo you want play again? (Y/N)').upper()
-	if again == 'N':
-		break
+    again = input('\n\nDo you want play again? (Y/N)')
+    if again.upper() == 'N':
+        break
 
 print('Thank for you play. Hope you enjoy it')
 saveScore(scorePlayer, scoreComputer)
